ef3f98d9777f2bbbbd1c81b34c71f86b869f80cd
Helios15 <ntak64k1@gmail.com>
2023-05-30 14:39:14 +0700

    add git hub private token

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,5 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  // ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDibMdS+ZHibdJqvqBe038V71fgYbrrSWxLeK8wiJg4gbjZuVsK0ky7N6WgSrxXN0PEA/p1ScsuwrTTQL50RafCLXPPgwDJ/256/ACbYRkWfjjkbtA6DbsZFDAgJh5/7lwjvVXiGddv3KVvRtuo+iO86YG5e2zuGZtlNed6pWgg3vdASai3Y1lr2SSESZ0J5IY9TzhLhlCUyht7GPcqInheo3A9Ve2H9sJetuctlpUYWMAqZUtZ2PVVXcH4dQvm2WnB2GdI+UGkH4EnzdQYMmeGb6kqWL3Cp14ny5vnASDG3OLqyXjy8+bUNkbISbGidL2riFysVRMHfjtHKiaOkXpEnW/RGULKwhJqhC+1fAqx/YxsKUTzgRteecT10+DWj9kTGYCH2Wd/qdtFk5fDhq0Yqmowui0jcxHhqEd+ka/cYftrJ4WKO3gecqSpqIc/xNnH88dv3LmhPIBrc4i6XvLioaPPObo68OWWQfnjIKzEjbTWaSfakdsZJaAUxExcMAH6egSlmnaIIFh5BJxMJx0J3/cGR4HyiI1ETOixwaFEjQs+p9PJj5kqzfyRwRxf4ZsRNecjpV7naBhfXMmQyjrRB8snjaGAmLjQ6cTm8eAvGWPamtCADSgRQs+ELR/+3Pda1gMQZWOg22/N+CE0C7M04oUwWCcNOfX8jB+L2n/C1Q== ubuntu@ip-172-31-20-181
 }
